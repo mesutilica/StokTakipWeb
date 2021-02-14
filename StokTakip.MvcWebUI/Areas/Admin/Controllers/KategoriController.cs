e312ed035d09e0aea3289e40c72cba430a4f2121
mesutilica <mesut.ilica@gmail.com>
2021-02-14 16:18:06 +0300

    14-02-21

---
+++ StokTakip.MvcWebUI/Areas/Admin/Controllers/KategoriController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StokTakip.Entities;
+using StokTakip.MvcWebUI.Data;
+
+namespace StokTakip.MvcWebUI.Areas.Admin.Controllers
+{
+    public class KategoriController : Controller
+    {
+        private StokTakipMvcWebUIContext db = new StokTakipMvcWebUIContext();
+
+        // GET: Admin/Kategoris
+        public ActionResult Index()
+        {
+            return View(db.Kategori.ToList());
+        }
+
+        // GET: Admin/Kategoris/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = db.Kategori.Find(id);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);
+        }
+
+        // GET: Admin/Kategoris/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Kategoris/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,KategoriAdi,KategoriAciklamasi,EklenmeTarihi,Aktif")] Kategori kategori)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kategori.Add(kategori);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kategori);
+        }
+
+        // GET: Admin/Kategoris/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = db.Kategori.Find(id);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);
+        }
+
+        // POST: Admin/Kategoris/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,KategoriAdi,KategoriAciklamasi,EklenmeTarihi,Aktif")] Kategori kategori)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kategori).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kategori);
+        }
+
+        // GET: Admin/Kategoris/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = db.Kategori.Find(id);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);
+        }
+
+        // POST: Admin/Kategoris/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kategori kategori = db.Kategori.Find(id);
+            db.Kategori.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
