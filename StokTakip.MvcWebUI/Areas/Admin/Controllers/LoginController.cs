c314fe55d5cdaf4dd20085d3f774d6fe8d337caf
mesutilica <mesut.ilica@gmail.com>
2021-02-14 15:12:58 +0300

    14-02-21

---
+++ StokTakip.MvcWebUI/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using StokTakip.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StokTakip.MvcWebUI.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Kullanici kullanici)
+        {
+            return View(kullanici);
+        }
+    }
+}
---
+++ StokTakip.MvcWebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StokTakip.MvcWebUI.MvcApplication" Language="C#" %>
